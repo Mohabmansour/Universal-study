7509c875347d391b35c31005bf2e9471acd67d2c
Mohab M.Mansour <110535913+Mohabmansour@users.noreply.github.com>
2024-02-27 16:04:47 +0200

    header edits

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,16 +1,24 @@
 import { Component } from '@angular/core';
 import { AnimationItem } from 'lottie-web';
 import { AnimationOptions } from 'ngx-lottie';
+import { LangService } from 'src/assets/services/lang.service';
+
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.scss'],
 })
 export class HeaderComponent {
+  constructor(private _LangService:LangService){
+
+  }
   options: AnimationOptions = {
     path: '../../../assets/images/Hero Section Image.json',
   };
-
+routing(path:string)
+{
+  this._LangService.scrollToTop(path)
+}
   animationCreated(animationItem: AnimationItem): void {
     console.log(animationItem);
   }
