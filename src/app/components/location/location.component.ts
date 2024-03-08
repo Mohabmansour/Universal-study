c2ae129822fdb74c969e0576f0b72fc02c3b3934
Mohab M.Mansour <110535913+Mohabmansour@users.noreply.github.com>
2024-03-08 12:00:45 +0200

    add maps

--- src/app/components/location/location.component.ts
+++ src/app/components/location/location.component.ts
@@ -1,4 +1,6 @@
+import { LangService } from 'src/assets/services/lang.service';
 import { Component } from '@angular/core';
+
 
 @Component({
   selector: 'app-location',
@@ -6,5 +8,8 @@ import { Component } from '@angular/core';
   styleUrls: ['./location.component.scss']
 })
 export class LocationComponent {
+  constructor(public _LangService:LangService){
+
+  }
 
 }
